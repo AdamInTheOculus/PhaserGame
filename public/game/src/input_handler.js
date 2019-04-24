33bd4369b7049351305c33b3da442312a629a1fd
AdamInTheOculus <a.ta.sinclair@gmail.com>
2019-04-24 13:59:46 -0400

    Created InputHandler class; added shared folder with constants

---
+++ public/game/src/input_handler.js
@@ -0,0 +1,40 @@
+/**
+ * @author   AdamInTheOculus
+ * @date     April 24th 2019
+ * @purpose  Class that handles all gamepad/keyboard/mouse input.
+ */
+
+class InputHandler {
+
+    constructor(phaserInput) {
+        this.phaser = phaserInput;
+    }
+
+    getState() {
+
+        /**
+         * In order for Gamepad input to be properly registered, we must manually update input.
+         * This is because of an InputPlugin bug -- not having InputPlugin.update() called automatically every frame.
+         *
+         * @author  AdamInTheOculus
+         * @date    April 7th 2019
+         * @see     https://github.com/photonstorm/phaser/issues/4414#issuecomment-480515615
+        **/
+        this.phaser.update();
+
+        let inputState = {
+            jump: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W),
+            jump2: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE),
+            fire: this.input.activePointer,
+            left: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A),
+            right: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D),
+            down: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S),
+            gamepad: this.input.gamepad.gamepads[0]
+        };
+
+        return inputState;
+    }
+
+}
+
+export default InputHandler;
---
+++ shared/constants.js
@@ -0,0 +1,10 @@
+/**
+ * @author   AdamInTheOculus
+ * @date     April 24th 2019
+ * @purpose  Contains all constant values across server and client.
+ */
+
+export const MOVE_LEFT = 1;
+export const MOVE_RIGHT = 2;
+export const JUMP = 3;
+export const IDLE = 4;
