03d2f470651a991ac30d1f824cd477814e964b95
jonmacphers <jonmacphers@gmail.com>
2019-04-24 19:38:03 -0400

    fixed canvas sizing

--- public/game/src/game.js
+++ public/game/src/game.js
@@ -14,7 +14,7 @@ import GUIScene from './scenes/GUIScene.js';
 const config = {
     type: Phaser.AUTO,    // Attempts to load WebGL. If it fails then load Canvas.
     width: window.innerWidth*window.devicePixelRatio,
-    height: window.innerWidth*window.devicePixelRatio,
+    height: window.innerHeight*window.devicePixelRatio,
     physics: {
         default: 'arcade',
         arcade: {
