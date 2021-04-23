1383c834d77d62045478f9da4d7e2e64665425d7
Adam Sinclair <a.ta.sinclair@gmail.com>
2021-04-22 22:16:15 -0700

    Committing new socket.io version

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
-const io = require('socket.io').listen(server);
+const io = require('socket.io')(server);
 const port = (process.env.PORT || 9001);
 
 const GameManager = require('./GameManager.js');
